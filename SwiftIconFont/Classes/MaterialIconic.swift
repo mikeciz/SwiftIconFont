866b76c297cafeac59c8e281bd5873606f536359
Mike Cizenski <mikeciz@gmail.com>
2016-12-09 08:09:50 -0500

    Material Iconic Font Added

--- SwiftIconFont/Classes/MaterialIconic.swift
+++ SwiftIconFont/Classes/MaterialIconic.swift
@@ -1,8 +1,9 @@
 //
-//  Material-Iconic.swift
+//  MaterialIcon.swift
+//  Exa
 //
-//
-//  Created by Mike on 12/9/16.
+//  Created by Sedat Gökbek ÇİFTÇİ on 08/07/16.
+//  Copyright © 2016 Sedat Gökbek ÇİFTÇİ. All rights reserved.
 //
 
 import UIKit
